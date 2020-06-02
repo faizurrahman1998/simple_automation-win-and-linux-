ef965b8d45260cda94298563f4bfa877a3f22f20
Fayez_blank <faizurrahman.fayez@gmail.com>
2020-06-02 21:17:04 +0600

    automation for opening jupyter notebook in a specific dir and opening a folder with course videos

---
+++ automation_101.py
@@ -0,0 +1,5 @@
+import subprocess
+
+subprocess.Popen(["xdg-open", "/media/fayez_blank/Coding_stuffs/Courses & Classes/Udemy-Complete Machine Learning and Data Science Zero to Mastery 2020"])
+subprocess.Popen(["jupyter", "notebook"], cwd = "/home/fayez_blank/Coding/PythonWorks/jupyter_projects")
+
